fa5ea32374ff5a71be4041d0f5946fc35585dda3
UNR-kkentley <134725086+UNR-kkentley@users.noreply.github.com>
2024-04-30 15:27:24 -0700

    Main backup LCD and DHT

--- Main Backup/CPE301_FinalProject_May2024.ino
+++ Main Backup/CPE301_FinalProject_May2024.ino
@@ -1,7 +1,31 @@
+
 // CPE Final Project April 2024
 //Kieran Kentley, Knight Fevella Potes, Eric Cordova Gonzalez, Abigail Ganze
 
 //include libraries that are allowed
+#include <Stepper.h> //Includes the Arduino Stepper Library
+#include <LiquidCrystal.h> //LCD Library
+#include <RTClib.h> //RTC
+#include <Wire.h> //I2C
+#include <DHT.h> //Humidity and Temp
+#include <DHT_U.h>
+
+#define DHTPIN 13          // Define the pin for DHT
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor
+
+
+
+//const int stepsPerRevolution = 2038;
+
+//Stepper myStepper = Stepper(stepsPerRevolution, 29, 27, 25, 23);
+
+// LCD pins <--> Arduino pins
+const int RS = 49, RW = 48, E = 47, D4 = 53, D5 = 51, D6 = 50, D7 = 52;
+LiquidCrystal lcd(RS, RW, E, D4, D5, D6, D7);
+
+void U0init(int U0baud);
+void adc_init();
 
 ////PORT B INPUT/OUTPUT
 volatile unsigned char* port_b = (unsigned char*) 0x25;
@@ -28,10 +52,10 @@ volatile unsigned char* port_a = (unsigned char*) 0x22;
 volatile unsigned char* ddr_a = (unsigned char*) 0x21;
 volatile unsigned char* pin_a = (unsigned char*) 0x20;
 
-//Place holder for Analog?
-//volatile unsigned char* port_b = (unsigned char*) 0x25;
-//volatile unsigned char* ddr_b = (unsigned char*) 0x24;
-//volatile unsigned char* pin_b = (unsigned char*) 0x23;
+//PORT L INPUT/OUTPUT
+volatile unsigned char* port_l = (unsigned char*) 0x10B;
+volatile unsigned char* ddr_l = (unsigned char*) 0x10A;
+volatile unsigned char* pin_l = (unsigned char*) 0x109;
 
 volatile unsigned char* myTCCR1A = (unsigned char*) 0x80;
 volatile unsigned char* myTCCR1B = (unsigned char*) 0x81;
@@ -45,13 +69,193 @@ volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
 
+#define RDA 0x80
+#define TBE 0x20  
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
 
 void setup() {
-  // put your setup code here, to run once:
+  U0init(9600);
+  adc_init();
+  dht.begin();
+  lcd.begin(16, 2); // set up number of columns and rows
+  
+
+ // rtc.begin(); //Start Real Time Clock
+
+//  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
+  //RTC.setHourMode(CLOCK_H12);
+  // if (RTC.getHourMode() == CLOCK_H12)
+  //{
+  //   RTC.setMeridiem(HOUR_PM);
+  //}
+
+ // RTC.setWeek(1);
+
+  //RTC.setDate(30,04,24);
+  //RTC.setTime(23,00,00);
+//}
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  
+  if (humidity == -999.0 || temperature == -999.0) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Failed to read");
+    lcd.setCursor(0, 1);
+    lcd.print("from DHT sensor!");
+    delay(2000);
+    return;
+  }
+
+  // Display temperature and humidity on LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print(" C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity: ");
+  lcd.print(humidity);
+  lcd.print("%");
+
+  delay(2000); // Delay for 2 seconds before next reading
+}
+  
+
+
+
+void set_PB_as_output(unsigned char pin_num){
+  *ddr_b |= (0x01 << pin_num);
+}
+
+void write_PB(unsigned char pin_num, unsigned char in_char){
+  
+  if((in_char & 0x01) == 0){
+      *port_b &= ~(0x01 << pin_num);
+  }
+  else{
+    *port_b |= (0x01 << pin_num);
+  }
+  
+}
+
+void set_PL_as_output(unsigned char pin_num){
+  *ddr_b |= (0x01 << pin_num);
+}
+
+//void write_PB(unsigned char pin_num, unsigned char in_char){
+  
+ // if((in_char & 0x01) == 0){
+  //    *port_b &= ~(0x01 << pin_num);
+ // }
+ // else{
+ //   *port_b |= (0x01 << pin_num);
+ // }
+  
+//}
+
+
+void adc_init()
+{
+  // setup the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 6 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 0-2 to 0 to set prescaler selection to slow reading
+  //the prescaler bit is the division factor between the clocks
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11110111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00001000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
+}
+
+void U0init(int U0baud)
+{
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+
+unsigned char U0kbhit()
+{
+  return *myUCSR0A & RDA;
+}
+
+void U0putchar(unsigned char U0pdata)
+{
+  while((*myUCSR0A & TBE)==0);
+  *myUDR0 = U0pdata;
+}
+
+
+void my_delay(unsigned int freq)
+{
+  // calc period
+  double period = 1.0/double(freq);
+  // 50% duty cycle
+  double half_period = period/ 2.0f;
+  // clock period def
+  double clk_period = 0.0000000625;
+  // calc ticks
+  unsigned int ticks = half_period / clk_period;
+  //Serial.println(ticks);
+  // stop the timer
+  *myTCCR1B &= 0xF8;
+  // set the counts
+  *myTCNT1 = (unsigned int) (65535-ticks);  //(65536 - ticks)
+  // start the timer
+  * myTCCR1B |= 0b00000010; 
+  //prescaler 0b00000001 - changed to be before TCCR1A is set to 0 for start
+  * myTCCR1A = 0x0;
+  
+  // wait for overflow
+  while((*myTIFR1 & 0x01)==0); // 0b 0000 0000
+  // stop the timer
+  *myTCCR1B &= 0xF8;   // 0b 0000 0000
+  // reset TOV           
+  *myTIFR1 |= 0x01;
 }
