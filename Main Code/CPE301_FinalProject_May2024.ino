9cb58cbc19b37574ca0921ab0cd21c77821666fc
UNR-kkentley <134725086+UNR-kkentley@users.noreply.github.com>
2024-05-12 10:35:48 -0700

    Final code. Added Abi's RTC code.
    Completed friday 5-10 for testing and video

--- Main Code/CPE301_FinalProject_May2024.ino
+++ Main Code/CPE301_FinalProject_May2024.ino
@@ -2,34 +2,36 @@
 // CPE Final Project May 2024
 //Kieran Kentley, Knight Fevella Potes, Eric Cordova Gonzalez, Abigail Ganze
 
-//include libraries that are allowed
-#include <Stepper.h> //Includes the Arduino Stepper Library
-#include <LiquidCrystal.h> //LCD Library
-#include <RTClib.h> //RTC
+#include <Stepper.h>
+#include <LiquidCrystal.h>
+#include <RTClib.h>
 #include <Wire.h> //I2C
-#include <DHT.h> //Humidity and Temp
+#include <DHT.h>
 #include <DHT_U.h>
 
-#define DHTPIN 13          // Define the pin for DHT
+#define DHTPIN 13
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor
+RTC_DS1307 RTC;
+DateTime currentTime;
 
 void U0init(int U0baud);
+void U0putchar(unsigned char U0pdata);
 void adc_init();
 unsigned int adc_read(unsigned char adc_channel_num);
 void enableLED(int color);
 void set_PE_as_input(unsigned char pin_num);
+void hInterrupt();
 
-// LCD pins <--> Arduino pins
+// LCD pins
 const int RS = 49, RW = 48, E = 47, D4 = 53, D5 = 51, D6 = 50, D7 = 52;
 LiquidCrystal lcd(RS, RW, E, D4, D5, D6, D7);
 
-unsigned long previousMillis = 0; //Previous state
-const long interval = 60000; //1-min delay
+unsigned long previousMillis = 0; //Previous time
+const long interval = 60000; //1-minute delay
 
-unsigned int adc_read(unsigned char adc_channel_num);
-unsigned int water_value = adc_read(7); //Read from ADC channel 7
-#define temp_value 20
+unsigned int water_value = adc_read(7); 
+#define temp_value 25 
 
 #define RDA 0x80
 #define TBE 0x20
@@ -39,12 +41,14 @@ unsigned int water_value = adc_read(7); //Read from ADC channel 7
 
 //Stepper potentiometer variables
 int previous_pot_val = 0;
-Stepper  myStepper(STEPS, 6, 8, 7, 9) ; //declaring  stepper motor  //this is the correct order 6,8,7,9
+Stepper  myStepper(STEPS, 6, 8, 7, 9); //this is the correct order 6,8,7,9
 
 //DC motor pins
 #define motorInput1 11
 #define motorInput2 12
 #define motorEnablePin 10
+
+#define DEBOUNCE_INT 780  // 50ms
 
 //---------------------------------------------------------
 
@@ -52,11 +56,6 @@ Stepper  myStepper(STEPS, 6, 8, 7, 9) ; //declaring  stepper motor  //this is th
 volatile unsigned char* port_b = (unsigned char*) 0x25;
 volatile unsigned char* ddr_b = (unsigned char*) 0x24;
 volatile unsigned char* pin_b = (unsigned char*) 0x23; // DHT PB7 D13 // DC controller (PB4-PB6) (D10-D12)
-
-////PORT H INPUT / OUTPUT
-volatile unsigned char* port_h = (unsigned char*) 0x102;
-volatile unsigned char* ddr_h = (unsigned char*) 0x101;
-volatile unsigned char* pin_h = (unsigned char*) 0x100;// DC controller (PH5-PH6) (D6-D7)
 
 ////PORT E INPUT/OUTPUT
 volatile unsigned char* port_e = (unsigned char*) 0x2E;
@@ -74,9 +73,9 @@ volatile unsigned char* ddr_a = (unsigned char*) 0x21;
 volatile unsigned char* pin_a = (unsigned char*) 0x20; //Water Sensor A7 PF7 //Green LED D23 PA1 //Yellow LED D24 PA2 //Blue LED D25 PA3 //Red LED D27 PA5
 
 // Port F Addresses for Input/Output
-volatile unsigned char* port_f = (unsigned char*) 0x31; // Data Register for PORT F
-volatile unsigned char* ddr_f = (unsigned char*) 0x30;  // Data Direction Register for PORT F
-volatile unsigned char* pin_f = (unsigned char*) 0x2F;  // Input Pins Address for PORT F
+volatile unsigned char* port_f = (unsigned char*) 0x31; 
+volatile unsigned char* ddr_f = (unsigned char*) 0x30;  
+volatile unsigned char* pin_f = (unsigned char*) 0x2F;  
 
 //PORT L INPUT/OUTPUT
 volatile unsigned char* port_l = (unsigned char*) 0x10B;
@@ -87,8 +86,8 @@ volatile unsigned char* myTCCR1A = (unsigned char*) 0x80;
 volatile unsigned char* myTCCR1B = (unsigned char*) 0x81;
 volatile unsigned char* myTCCR1C = (unsigned char*) 0x82;
 volatile unsigned char* myTIMSK1 = (unsigned char*) 0x6F;
-volatile unsigned int* myTCNT1  = (unsigned  int*) 0x84;
-volatile unsigned char* myTIFR1 =  (unsigned char*) 0x36;
+volatile unsigned int* myTCNT1  = (unsigned int*) 0x84;
+volatile unsigned char* myTIFR1 = (unsigned char*) 0x36;
 
 volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
@@ -100,13 +99,6 @@ volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
 volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
 volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
 volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
-
-volatile unsigned char* myEIMSK = (unsigned char*) 0x1D;
-volatile unsigned char* myEIFR = (unsigned char*) 0x1C;
-volatile unsigned char* mySREG = (unsigned char*) 0x3F;
-volatile unsigned char* myEICRB = (unsigned char*) 0x6A;
-
-
 
 //------------------------------------------------------
 
@@ -125,52 +117,77 @@ void setup() {
   adc_init();
   dht.begin();
   lcd.begin(16, 2); // Set up the LCD
-  *ddr_a |= 0b00111110; // Set LED pins as output
-  myStepper.setSpeed(128) ;
+  *ddr_a |= 0b00111110; // Set LED pins PA# as outputs
+  myStepper.setSpeed(128);
+  Wire.begin();
+  RTC.begin();
 
-  set_PE_as_input(4); // Set up button as input
-
-  //DC motor pins
-  set_PB_as_output(11);
-  set_PB_as_output(12);
-  set_PB_as_output(10);
+  if (!RTC.isrunning()) {
+    RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 
   presentState = DISABLED_ST;
   nextState = DISABLED_ST;
   enableLED(presentState);
 
+  set_PE_as_input(4); // Set up button as input
+  set_PB_as_output(11);//DC motor pins
+  set_PB_as_output(12);
+  set_PB_as_output(10);
+
   attachInterrupt(digitalPinToInterrupt(2), hInterrupt, FALLING);
 }
-
 void loop() {
-  unsigned int water_value = adc_read(7); // Read from ADC channel 7
+  unsigned int water_value = adc_read(7);
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
+  currentTime = RTC.now();
 
-  //stepperMotor(1);
-  //dc_motor(1); //call function for motor control 1 - motor on 0 - motor off
+  if (presentState == RUNNING_ST) {
+    dc_motor(1);
+  } else {
+    dc_motor(0);
+  }
+
+  if (presentState == RUNNING_ST || presentState == IDLE_ST) {
+    stepperMotor(1);
+  } else {
+    stepperMotor(0);
+  }
 
   switch (presentState) {
     case DISABLED_ST:
       if (buttonPressed) {
         buttonPressed = false;
-        lcd.clear();
-        lcd.print("Disabled State");
         nextState = IDLE_ST;
       }
+      U0PrintTime("DISABLED STATE");
+      dc_motor(0);
+      lcd.clear();
+      lcd.print("Disabled");
       break;
 
     case IDLE_ST:
-      lcd.clear();
-      lcd.print("Temp: ");
-      lcd.print(temperature);
-      lcd.print(" C");
-      lcd.setCursor(0, 1);
-      lcd.print("Humidity: ");
-      lcd.print(humidity);
-      lcd.print("%");
-
+      htLCD(humidity, temperature, presentState);
+      U0PrintTime("IDLE STATE ");
+      dc_motor(0);
       if (buttonPressed) {
+        buttonPressed = false;
+        nextState = DISABLED_ST;
+      } else if (water_value <= 120) {
+        nextState = ERROR_ST;
+      } else if (water_value > 120 && temperature > temp_value) {
+        nextState = RUNNING_ST;
+      }
+      break;
+
+    case RUNNING_ST:
+      htLCD(humidity, temperature, presentState);
+      U0PrintTime("RUNNING STATE");
+      dc_motor(1);
+      if (temperature < temp_value) {
+        nextState = IDLE_ST;
+      } else if (buttonPressed) {
         buttonPressed = false;
         nextState = DISABLED_ST;
       } else if (water_value <= 120) {
@@ -178,24 +195,17 @@ void loop() {
       }
       break;
 
-    case RUNNING_ST:
-      if (water_value > 120 && temperature > 1) {
-        enableLED(RUNNING_ST); // Blue LED On
-
-      } else {
-        nextState = ERROR_ST;
-      }
-      break;
-
     case ERROR_ST:
+      U0PrintTime("Water low, ERROR STATE");
+      dc_motor(0);
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Error Message:");
       lcd.setCursor(0, 1);
       lcd.print("Water Level Low");
-      enableLED(ERROR_ST); // Red LED On
-
-      if (water_value > 120) {
+      enableLED(ERROR_ST);
+      if (buttonPressed) {
+        buttonPressed = false;
         nextState = DISABLED_ST;
       }
       break;
@@ -204,50 +214,93 @@ void loop() {
   if (nextState != presentState) {
     presentState = nextState;
     enableLED(presentState);
+    if (presentState != IDLE_ST && presentState != RUNNING_ST) {
+      stepperMotor(0); // Turn off stepper motor when not in IDLE_ST or RUNNING_ST
+    }
   }
 }
-//--------------------------------------------------------------------------------
 
+void U0PrintTime(String s) {
+  String U0Send = String(currentTime.year());
+  for (int i = 0; i < U0Send.length(); i++) {
+    U0putchar(U0Send[i]);
+  }
+
+  U0putchar('/');
+  U0Send = currentTime.month();
+
+  for (int i = 0; i < U0Send.length(); i++) {
+    U0putchar(U0Send[i]);
+  }
+
+  U0putchar('/');
+  U0Send = currentTime.day();
+
+  for (int i = 0; i < U0Send.length(); i++) {
+    U0putchar(U0Send[i]);
+  }
+
+  U0putchar(' ');
+  U0Send = currentTime.hour();
+
+  for (int i = 0; i < U0Send.length(); i++) {
+    U0putchar(U0Send[i]);
+  }
+
+  U0putchar(':');
+  U0Send = currentTime.minute();
+
+  for (int i = 0; i < U0Send.length(); i++) {
+    U0putchar(U0Send[i]);
+  }
+
+  U0putchar(':');
+  U0Send = currentTime.second();
+
+  for (int i = 0; i < U0Send.length(); i++) {
+    U0putchar(U0Send[i]);
+  }
+
+  s += "\n";
+
+  for (int i = 0; i < s.length(); i++) {
+    U0putchar(s[i]);
+  }
+}
+
+//--------------------------------------------------------------------------------
 
 void adc_init()
 {
   // setup the A register
-  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
-  *my_ADCSRA &= 0b11011111; // clear bit 6 to 0 to disable the ADC trigger mode
-  *my_ADCSRA &= 0b11110111; // clear bit 5 to 0 to disable the ADC interrupt
-  *my_ADCSRA &= 0b11111000; // clear bit 0-2 to 0 to set prescaler selection to slow reading
-  //the prescaler bit is the division factor between the clocks
-  // setup the B register
-  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
-  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
-  // setup the MUX Register
-  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
-  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
-  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
-  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+  *my_ADCSRA |= 0b10000000; 
+  *my_ADCSRA &= 0b11011111; 
+  *my_ADCSRA &= 0b11110111; 
+  *my_ADCSRA &= 0b11111000; 
+  
+  *my_ADCSRB &= 0b11110111; 
+  *my_ADCSRB &= 0b11111000; 
+  
+  *my_ADMUX  &= 0b01111111; 
+  *my_ADMUX  |= 0b01000000; 
+  *my_ADMUX  &= 0b11011111; 
+  *my_ADMUX  &= 0b11100000; 
 }
 
 unsigned int adc_read(unsigned char adc_channel_num)
 {
-  // clear the channel selection bits (MUX 4:0)
   *my_ADMUX  &= 0b11100000;
-  // clear the channel selection bits (MUX 5)
   *my_ADCSRB &= 0b11110111;
   // set the channel number
   if (adc_channel_num > 7)
   {
-    // set the channel selection bits, but remove the most significant bit (bit 3)
     adc_channel_num -= 8;
-    // set MUX bit 5
     *my_ADCSRB |= 0b00001000;
   }
-  // set the channel selection bits
   *my_ADMUX  += adc_channel_num;
-  // set bit 6 of ADCSRA to 1 to start a conversion
   *my_ADCSRA |= 0x40;
-  // wait for the conversion to complete
   while ((*my_ADCSRA & 0x40) != 0);
-  // return the result in the ADC data register
+  // return the result
   return *my_ADC_DATA;
 }
 
@@ -274,34 +327,35 @@ void U0putchar(unsigned char U0pdata)
   *myUDR0 = U0pdata;
 }
 
+void htLCD(float humidity, float temp, int presentState) {
+  static unsigned long previousMillis = 0; //Previous state
+  const long interval = 60000; //1-minute delay
 
-void my_delay(unsigned int freq)
-{
-  // calc period
-  double period = 1.0 / double(freq);
-  // 50% duty cycle
-  double half_period = period / 2.0f;
-  // clock period def
-  double clk_period = 0.0000000625;
-  // calc ticks
-  unsigned int ticks = half_period / clk_period;
-  //Serial.println(ticks);
-  // stop the timer
-  *myTCCR1B &= 0xF8;
-  // set the counts
-  *myTCNT1 = (unsigned int) (65535 - ticks); //(65536 - ticks)
-  // start the timer
-  * myTCCR1B |= 0b00000010;
-  //prescaler 0b00000001 - changed to be before TCCR1A is set to 0 for start
-  * myTCCR1A = 0x0;
+  static int previousState = DISABLED_ST;
+  unsigned long currentMillis = millis();
 
-  // wait for overflow
-  while ((*myTIFR1 & 0x01) == 0); // 0b 0000 0000
-  // stop the timer
-  *myTCCR1B &= 0xF8;   // 0b 0000 0000
-  // reset TOV
-  *myTIFR1 |= 0x01;
+  if (presentState == RUNNING_ST || presentState == IDLE_ST) {
+    if ((presentState != previousState) || (currentMillis - previousMillis >= interval)) {
+      previousMillis = currentMillis; 
+      previousState = presentState;
+      lcd.setCursor(0, 0);
+      lcd.print("Temp: ");
+      lcd.print(temp);
+      lcd.print("C");
+      lcd.setCursor(0, 1);
+      lcd.print("Humidity: ");
+      lcd.print(humidity);
+      lcd.print("%");
+
+    }
+  }
+
+  else if (presentState == DISABLED_ST){
+    lcd.clear();
+    lcd.print("Disabled");
+  }
 }
+//------------------
 
 void enableLED(int state) {
   *port_a &= ~(0b00111110); // Turn off all LEDs
@@ -323,19 +377,7 @@ void enableLED(int state) {
 
 //------------------------------------------------------
 
-
-
-void stepperMotor(int stepperCondition) {
-  if (stepperCondition == 1) {
-    int current_pot_val = analogRead(9); //adc_read does not work, stepper does not loop if using adc_read
-
-    myStepper.step(current_pot_val - previous_pot_val);
-
-    previous_pot_val = current_pot_val;
-  }
-}
-
-void set_PF_as_input(unsigned char pin_num) { //function to set port B to output
+void set_PF_as_input(unsigned char pin_num) {
   *ddr_f &= ~(1 << pin_num);
 }
 
@@ -344,53 +386,121 @@ void set_PE_as_input(unsigned char pin_num) {
 
 }
 
-void set_pe_pullup(unsigned char pin_num) {
-  *port_e |= (1 << pin_num);
-
-}
-
-void set_PB_as_output(unsigned char pin_num) { //function to set port B to output
+void set_PB_as_output(unsigned char pin_num) {
   *ddr_b |= (0x01 << pin_num);
 }
 
-void write_PB_high(unsigned char pin_num) { //function to set specified port B pin HIGH
+void write_PB_high(unsigned char pin_num) {
   *port_b |= (0x01 << pin_num);
 }
 
-void set_PB_as_input(unsigned char pin_num) { //function to set port B to output
+void set_PB_as_input(unsigned char pin_num) {
   *ddr_b &= ~(1 << pin_num);
 }
 
-void write_PB_low(unsigned char pin_num) { //function to set specified port B pin LOW
+void write_PB_low(unsigned char pin_num) {
   *port_b &= ~(1 << pin_num);
 }
 
-void write_pin(unsigned char pin_num, int value) { //function to set specified value at specified port&pin
-  *port_b = (*port_b & ~(1 << pin_num)) | value;
-}
+void dc_motor(int motorCondition) {
+  static bool motorRunning = false;
 
-void dc_motor(int motorCondition) { //function to control dc motor output
-
-  float temperature = (dht.readTemperature() * 9 / 5 + 32); // convert temperature reading to fahrenheit
-
-  if (motorCondition == 1) {
-    while (temperature > 70.0) { // dc motor will only power on if DHT11 temperature is above threshold
-      write_PB_high(motorInput1); // provide power to dc motor
-      //write_PB_low(motorInput2);
-      write_pin(motorEnablePin, 225); // set dc motor speed via enable pin on L293D chip
-
-      temperature = (dht.readTemperature() * 9 / 5 + 32); //re-measure temperature for while loop condition
-    }
+  //motorEnablePin
+  //motorInput1,2
+  if (motorCondition == 1 && !motorRunning) {
+    write_PB_high(11);
+    write_PB_low(12);
+    analogWrite(10, 225);
+    U0Send("Fan motor turned on ");
+    motorRunning = true;
+  } else if (motorCondition == 0 && motorRunning) {
+    write_PB_low(10);
+    U0Send("Fan motor turned off ");
+    motorRunning = false;
   }
 }
 
-void hInterrupt()
-{
-  static unsigned long lastInterruptTime = 0;
-  unsigned long interruptTime = millis();
+void stepperMotor(int stepperState) {
+  static int lastSeg = -1;
+  int current_pot_val = adc_read(9);
+  int segSize = 1023 / 15;
+  int currentSeg = current_pot_val / segSize;
 
-  if (interruptTime - lastInterruptTime > 50) {
-    buttonPressed = true;
-    lastInterruptTime = interruptTime;
+  if (stepperState == 1 && currentSeg != lastSeg) {
+    int stepCount = (currentSeg - lastSeg) * step_move;  // Calculate steps to move
+    myStepper.step(stepCount);
+    U0Send("Stepper motor moved position");
+    lastSeg = currentSeg;
+  } else if (stepperState == 0) {
+    lastSeg = -1;  // Reset
   }
+}
+
+void U0Send (String s) {
+  String U0Print = String(currentTime.year());
+  for (int i = 0; i < U0Print.length(); i++) {
+    U0putchar(U0Print[i]);
+  }
+  
+  U0putchar('/');
+  U0Print = currentTime.month();
+
+  for (int i = 0; i < U0Print.length(); i++) {
+    U0putchar(U0Print[i]);
+  }
+
+  U0putchar('/');
+  U0Print = currentTime.day();
+
+  for (int i = 0; i < U0Print.length(); i++) {
+    U0putchar(U0Print[i]);
+  }
+
+  U0putchar(' ');
+  U0Print = currentTime.hour();
+
+  for (int i = 0; i < U0Print.length(); i++) {
+    U0putchar(U0Print[i]);
+  }
+
+  U0putchar(':');
+  U0Print = currentTime.minute();
+
+  for (int i = 0; i < U0Print.length(); i++) {
+    U0putchar(U0Print[i]);
+  }
+
+  U0putchar(':');
+  U0Print = currentTime.second();
+
+  for (int i = 0; i < U0Print.length(); i++) {
+    U0putchar(U0Print[i]);
+  }
+
+  s += "\n";
+
+  for (int i = 0; i < s.length(); i++) {
+    U0putchar(s[i]);
+  }
+}
+
+void InterruptTimer() {
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 65535 - DEBOUNCE_INT;  // Set the timer count
+  TIMSK1 &= ~(1 << TOIE1);
+  TCCR1B |= (1 << CS12);    // Set the prescaler and start timer
+  TIMSK1 |= (1 << TOIE1);
+}
+
+ISR(TIMER1_OVF_vect) {
+  TCNT1 = 65535 - DEBOUNCE_INT;  // Reload timer
+  buttonPressed = true;
+  TIMSK1 &= ~(1 << TOIE1);
+}
+//Checking this to make sure the debounce is good, seeing mixed results on scope
+void hInterrupt() {
+  TIMSK1 &= ~(1 << TOIE1);
+  TCNT1 = 65535 - DEBOUNCE_INT;  // Restart the debounce timer
+  TIMSK1 |= (1 << TOIE1);
 }
